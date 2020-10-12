0e0bb7439e910a35c8a65ff5b5660ed06666ec76
Velican Akcakaya <velican@akcakaya.net>
2020-10-12 17:50:41 +0200

    feat(pdf-compare): implement download from GCS bucket

--- src/comparison/comparison.module.ts
+++ src/comparison/comparison.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PdfCompareService } from './pdf-compare/pdf-compare.service';
+import { PrismaModule } from '../prisma/prisma.module';
+import { GoogleModule } from '../google/google.module';
 
 @Module({
+  imports: [PrismaModule, GoogleModule],
   providers: [PdfCompareService],
   exports: [PdfCompareService],
 })
--- src/comparison/pdf-compare/pdf-compare.service.ts
+++ src/comparison/pdf-compare/pdf-compare.service.ts
@@ -1,12 +1,39 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../../prisma/prisma.service';
+import { GoogleStorageService } from '../../google/google-storage/google-storage.service';
 
 @Injectable()
 export class PdfCompareService {
+  constructor(
+    private readonly prismaService: PrismaService,
+    private readonly googleStorageService: GoogleStorageService,
+  ) {}
+
+  async downloadFromGoogleCloudStorage(fileName: string): Promise<Buffer> {
+    return (
+      this.googleStorageService
+        .getPdfCompareBucket()
+        .file(fileName)
+        // Passing in no options downloads the files into memory.
+        .download({})
+        // Map the first and only child of data to a buffer.
+        .then(data => data[0])
+    );
+  }
+
   async compare(
     firstPdfFileId: string,
     secondPdfFileId: string,
-  ): Promise<Record<string, unknown>> {
+  ): Promise<void> {
+    const pdfFiles = await this.prismaService.pdfFile.findMany({
+      where: { id: { in: [firstPdfFileId, secondPdfFileId] } },
+    });
+
+    pdfFiles.forEach(async pdfFile => {
+      const bla = await this.downloadFromGoogleCloudStorage(pdfFile.remotePath);
+      bla;
+    });
+
     // @TODO Implement
-    return {};
   }
 }
